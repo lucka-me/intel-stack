91bac06651ca732a83a04b6442fbf8b52a673896
lucka-me <i@lucka.moe>
2023-09-27 20:31:34 +0800

    Remove debugging code

--- Web Extension/Shared/Resources/background.js
+++ Web Extension/Shared/Resources/background.js
@@ -1,7 +1,5 @@
 async function forwardMessage(request, sender, sendResponse) {
-    console.log("Received request: ", request);
     const response = await browser.runtime.sendNativeMessage(request);
-    console.log("Received response: ", response);
     sendResponse(response);
     return true;
 }
