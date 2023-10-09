af8c9d917b055ce3ab6854baa048f901e1063f34
lucka-me <i@lucka.moe>
2023-10-09 21:12:10 +0800

    Split ContentView.sidebarContent to SidebarView

--- App/Shared/Views/ContentView.swift
+++ App/Shared/Views/ContentView.swift
@@ -8,43 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    private enum SidebarSelection : Hashable {
-        case settings
-        case plugins(category: Plugin.Category)
-    }
-    
-    @AppStorage(UserDefaults.Key.scriptsEnabled) private var scriptsEnabled = false
-    
-    @Environment(\.scriptManager) private var scriptManager
-    
-    @State private var mainScriptVersion: String? = nil
-    @State private var sidebarSelection: SidebarSelection? = nil
+    @State private var sidebarSelection: SidebarView.Selection? = nil
     
     var body: some View {
         NavigationSplitView {
-            List(selection: $sidebarSelection) {
-                sidebarContent
-            }
-            #if os(macOS)
-            .listStyle(.sidebar)
-            .frame(minWidth: 200)
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button("Refresh", systemImage: "arrow.clockwise") {
-                        Task {
-                            await tryDownloadScripts()
-                        }
-                    }
-                    .disabled(scriptManager.status != .idle)
-                }
-            }
-            #else
-            .listStyle(.insetGrouped)
-            .refreshable {
-                await tryDownloadScripts()
-            }
-            #endif
-            .navigationTitle("Intel Stack")
+            SidebarView(selection: $sidebarSelection)
         } detail: {
             NavigationStack {
                 switch sidebarSelection {
@@ -64,73 +32,6 @@ struct ContentView: View {
         .frame(minHeight: 450)
         #endif
         .navigationSplitViewStyle(.balanced)
-        .onChange(of: scriptManager.status, initial: true) {
-            if scriptManager.status == .idle {
-                mainScriptVersion = ScriptManager.fetchMainScriptVersion()
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private var sidebarContent: some View {
-        Section {
-            if scriptManager.status == .idle, let mainScriptVersion {
-                Toggle("Enabled", systemImage: "power", isOn: $scriptsEnabled)
-                #if os(macOS)
-                    .toggleStyle(.switch)
-                #endif
-                Label(mainScriptVersion, systemImage: "scroll")
-                    .monospaced()
-            } else {
-                switch scriptManager.status {
-                case .idle:
-                    Label("Unavailable", systemImage: "xmark.octagon")
-                        .symbolRenderingMode(.multicolor)
-                    Button("Download", systemImage: "arrow.down") {
-                        Task {
-                            await tryDownloadScripts()
-                        }
-                    }
-                case .downloading:
-                    Label("Downloading", systemImage: "arrow.down.circle.dotted")
-                        .symbolRenderingMode(.multicolor)
-                        .symbolEffect(.pulse, options: .repeating)
-                    ProgressView(scriptManager.downloadProgress)
-                }
-            }
-        } header: {
-            Text("IITC Script")
-        } footer: {
-            Text("Pull to downlaod / update scripts.")
-        }
-        
-        Section {
-            NavigationLink(value: SidebarSelection.settings) {
-                Label("Settings", systemImage: "gear")
-            }
-        }
-        
-        Section {
-            if mainScriptVersion != nil {
-                ForEach(Plugin.Category.allCases, id: \.rawValue) { category in
-                    NavigationLink(value: SidebarSelection.plugins(category: category)) {
-                        Label(category.rawValue, systemImage: category.icon)
-                    }
-                }
-            }
-        } header: {
-            Text("Plugins")
-        }
-    }
-    
-    @MainActor
-    private func tryDownloadScripts() async {
-        guard scriptManager.status == .idle else { return }
-        do {
-            try await scriptManager.downloadScripts()
-        } catch {
-            print(error)
-        }
     }
 }
 
---
+++ App/Shared/Views/SidebarView.swift
@@ -0,0 +1,179 @@
+//
+//  SidebarView.swift
+//  Intel Stack
+//
+//  Created by Lucka on 2023-10-05.
+//
+
+import SwiftUI
+#if os(macOS)
+import SafariServices
+#endif
+
+struct SidebarView: View {
+    enum Selection : Hashable {
+        case settings
+        case plugins(category: Plugin.Category)
+    }
+    
+    static private let extensionId = "dev.lucka.IntelStack.WebExtension"
+    
+    @AppStorage(UserDefaults.Key.scriptsEnabled) private var scriptsEnabled = false
+    
+    @Binding private var selection: Selection?
+    
+    @Environment(\.scriptManager) private var scriptManager
+    #if os(macOS)
+    @Environment(\.scenePhase) private var scenePhase : ScenePhase
+    #endif
+    
+    @State private var mainScriptVersion: String? = nil
+    #if os(macOS)
+    @State private var extensionEnabled: Bool? = nil
+    #endif
+    
+    init(selection: Binding<Selection?>) {
+        self._selection = selection
+    }
+    
+    var body: some View {
+        List(selection: $selection) {
+            sidebarContent
+        }
+        #if os(macOS)
+        .listStyle(.sidebar)
+        .frame(minWidth: 200)
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button("Refresh", systemImage: "arrow.clockwise") {
+                    Task {
+                        await tryDownloadScripts()
+                    }
+                }
+                .disabled(scriptManager.status != .idle)
+            }
+        }
+        #else
+        .listStyle(.insetGrouped)
+        .refreshable {
+            await tryDownloadScripts()
+        }
+        #endif
+        .navigationTitle("Intel Stack")
+        .onChange(of: scriptManager.status, initial: true) {
+            if scriptManager.status == .idle {
+                mainScriptVersion = ScriptManager.fetchMainScriptVersion()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var sidebarContent: some View {
+        Section {
+            scriptSectionContent
+        } header: {
+            Text("IITC Script")
+        } footer: {
+            Text("Pull to downlaod / update scripts.")
+        }
+        
+        #if os(macOS)
+        Section {
+            Button(extensionStateTextKey, systemImage: extensionStateIcon) {
+                SFSafariApplication.showPreferencesForExtension(withIdentifier: Self.extensionId)
+            }
+            .buttonStyle(.link)
+            .foregroundStyle(extensionSateColor)
+            .onChange(of: scenePhase, initial: true, fetchExtensionState)
+        } header: {
+            Text("Safari Extension")
+        }
+        #endif
+        
+        Section {
+            if mainScriptVersion != nil {
+                ForEach(Plugin.Category.allCases, id: \.rawValue) { category in
+                    NavigationLink(value: Selection.plugins(category: category)) {
+                        Label(category.rawValue, systemImage: category.icon)
+                    }
+                }
+            }
+        } header: {
+            Text("Plugins")
+        }
+    }
+    
+    @ViewBuilder
+    private var scriptSectionContent: some View {
+        Toggle("Enabled", systemImage: "power", isOn: $scriptsEnabled)
+            #if os(macOS)
+            .toggleStyle(.switch)
+            #endif
+            .disabled(mainScriptVersion == nil)
+        if let mainScriptVersion {
+            if scriptManager.status == .idle {
+                Label(mainScriptVersion, systemImage: "scroll")
+                    .monospaced()
+            }
+        } else {
+            Label("Unavailable", systemImage: "xmark.octagon")
+                .symbolRenderingMode(.multicolor)
+        }
+        
+        NavigationLink(value: Selection.settings) {
+            Label("Settings", systemImage: "gear")
+        }
+        
+        if scriptManager.status == .downloading {
+            Label("Downloading", systemImage: "arrow.down.circle.dotted")
+                .symbolRenderingMode(.multicolor)
+                .symbolEffect(.pulse, options: .repeating)
+            ProgressView(scriptManager.downloadProgress)
+        }
+    }
+    
+    #if os(macOS)
+    private var extensionStateTextKey: LocalizedStringKey {
+        switch extensionEnabled {
+        case true: "Enabled"
+        case false: "Disabled"
+        default: "Unknown"
+        }
+    }
+
+    private var extensionStateIcon: String {
+        switch extensionEnabled {
+        case true: "checkmark.circle"
+        case false: "xmark.octagon"
+        default: "questionmark.diamond"
+        }
+    }
+    
+    private var extensionSateColor: Color {
+        switch extensionEnabled {
+        case true: .green
+        case false: .red
+        default: .orange
+        }
+    }
+    #endif
+    
+    @MainActor
+    private func tryDownloadScripts() async {
+        do {
+            try await scriptManager.downloadScripts()
+        } catch {
+            print(error)
+        }
+    }
+    
+    #if os(macOS)
+    private func fetchExtensionState() {
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: Self.extensionId) { state, error in
+            DispatchQueue.main.async {
+                extensionEnabled = state?.isEnabled
+            }
+        }
+    }
+    #endif
+}
