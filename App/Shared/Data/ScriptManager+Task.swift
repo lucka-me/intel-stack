lucka-me/intel-stack

commit 41498dbf71b667fb99e7cd42226783213f6fb984

Add simple HTTP methods and errors for URLSession

App/Shared/Data/ScriptManager+Task.swift
@@ -13,24 +13,6 @@ extension ScriptManager {
         case release = "release"
         case beta = "beta"
     }
-
-    enum TaskError: Error, LocalizedError {
-        case invalidHTTPResponse(statusCode: Int)
-        
-        var errorDescription: String? {
-            switch self {
-            case .invalidHTTPResponse(let statusCode):
-                return .init(localized: "ScriptManager.TaskError.InvalidHTTPResponse \(statusCode)")
-            }
-        }
-        
-        var failureReason: String? {
-            switch self {
-            case .invalidHTTPResponse(let statusCode):
-                return HTTPURLResponse.localizedString(forStatusCode: statusCode)
-            }
-        }
-    }
 }
 
 extension ScriptManager {
@@ -103,7 +85,8 @@ extension ScriptManager {
             .appending(path: channel.rawValue)
             .appending(path: FileConstants.mainScriptFilename)
             .appendingPathExtension("user.js")
-        let (temporaryURL, response) = try await URLSession.shared.download(from: downloadURL)
+
+        let temporaryURL = try await URLSession.shared.download(from: downloadURL)
         
         let fileManager = FileManager.default
         var succeed = false
@@ -111,11 +94,6 @@ extension ScriptManager {
             if !succeed {
                 try? fileManager.removeItem(at: temporaryURL)
             }
-        }
-        
-        guard let httpResponse = response as? HTTPURLResponse else { return }
-        guard httpResponse.statusCode == 200 else {
-            throw TaskError.invalidHTTPResponse(statusCode: httpResponse.statusCode)
         }
         
         let content = try String(contentsOf: temporaryURL)
@@ -131,11 +109,9 @@ extension ScriptManager {
     func downloadInternalPlugin(_ filename: String, from channel: BuildChannel) async throws {
         let downloadURL = Self.websiteBuildURL
             .appending(path: channel.rawValue)
-            .appending(path: "plugins")
-            .appending(path: filename)
+            .appending(path: FileConstants.mainScriptFilename)
             .appendingPathExtension("user.js")
-        
-        let (temporaryURL, response) = try await URLSession.shared.download(from: downloadURL)
+        let temporaryURL = try await URLSession.shared.download(from: downloadURL)
         
         let fileManager = FileManager.default
         var succeed = false
@@ -143,11 +119,6 @@ extension ScriptManager {
             if !succeed {
                 try? fileManager.removeItem(at: temporaryURL)
             }
-        }
-        
-        guard let httpResponse = response as? HTTPURLResponse else { return }
-        guard httpResponse.statusCode == 200 else {
-            throw TaskError.invalidHTTPResponse(statusCode: httpResponse.statusCode)
         }
         
         let content = try String(contentsOf: temporaryURL)
@@ -198,16 +169,10 @@ fileprivate extension ScriptManager {
     private func updateExternal(plugin: Plugin, in externalURL: URL) async throws {
         // TODO: Use updateURL to fetch metadata
         guard let downloadURL = plugin.downloadURL else { return }
-
-        let (temporaryURL, response) = try await URLSession.shared.download(from: downloadURL)
+        let temporaryURL = try await URLSession.shared.download(from: downloadURL)
 
         let fileManager = FileManager.default
         defer { try? fileManager.removeItem(at: temporaryURL) }
-        
-        guard let httpResponse = response as? HTTPURLResponse else { return }
-        guard httpResponse.statusCode == 200 else {
-            throw TaskError.invalidHTTPResponse(statusCode: httpResponse.statusCode)
-        }
         
         let destinationURL = externalURL
             .appending(path: plugin.filename)
@@ -221,7 +186,9 @@ fileprivate extension ScriptManager {
             return
         }
         
-        try fileManager.replaceItem(at: destinationURL, withItemAt: temporaryURL, backupItemName: nil, resultingItemURL: nil)
+        try fileManager.replaceItem(
+            at: destinationURL, withItemAt: temporaryURL, backupItemName: nil, resultingItemURL: nil
+        )
         plugin.update(from: metadata)
     }
 }
@@ -231,7 +198,9 @@ fileprivate extension ScriptManager {
         get throws {
             try modelContext.fetch(
                 FetchDescriptor<Plugin>(
-                    predicate: #Predicate { !$0.isInternal && $0.version != nil && ($0.downloadURL != nil || $0.updateURL != nil) }
+                    predicate: #Predicate {
+                        !$0.isInternal && $0.version != nil && ($0.downloadURL != nil || $0.updateURL != nil)
+                    }
                 )
             )
         }

App/Shared/Extensions/URLSession+HTTP.swift
@@ -1,0 +1,58 @@
+//
+//  URLSession+HTTP.swift
+//  Intel Stack
+//
+//  Created by Lucka on 2024-01-26.
+//
+
+import Foundation
+
+enum HTTPResponseError : LocalizedError {
+    case invalidResponseType
+    case invalidHTTPResponse(statusCode: Int)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidResponseType:
+            return .init(localized: "HTTPResponseError.InvalidResponseType")
+        case .invalidHTTPResponse(let statusCode):
+            return .init(localized: "HTTPResponseError.InvalidHTTPResponse \(statusCode)")
+        }
+    }
+    
+    var failureReason: String? {
+        switch self {
+        case .invalidResponseType:
+            return .init(localized: "HTTPResponseError.InvalidResponseType.Reason")
+        case .invalidHTTPResponse(let statusCode):
+            return HTTPURLResponse.localizedString(forStatusCode: statusCode)
+        }
+    }
+}
+
+extension URLSession {
+    func download(from url: URL) async throws -> URL {
+        let (temporaryURL, response) = try await download(from: url)
+        let fileManager = FileManager.default
+        var succeed = false
+        defer {
+            if !succeed {
+                try? fileManager.removeItem(at: temporaryURL)
+            }
+        }
+        try Self.checkHTTP(response: response)
+        succeed = true
+        return temporaryURL
+    }
+}
+
+fileprivate extension URLSession {
+    static func checkHTTP(response: URLResponse) throws {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw HTTPResponseError.invalidResponseType
+        }
+        guard httpResponse.statusCode == 200 else {
+            throw HTTPResponseError.invalidHTTPResponse(statusCode: httpResponse.statusCode)
+        }
+    }
+}

App/Shared/Localization/Localizable.xcstrings
@@ -257,22 +257,6 @@
         }
       }
     },
-    "AddPluginView.TaskError.InvalidHTTPResponse %lld" : {
-      "localizations" : {
-        "en" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "Invalid HTTP response code: %lld"
-          }
-        },
-        "zh-Hans" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "无效的HTTP回应状态码：%lld"
-          }
-        }
-      }
-    },
     "AddPluginView.TaskError.InvalidURL" : {
       "localizations" : {
         "en" : {
@@ -365,6 +349,54 @@
           "stringUnit" : {
             "state" : "translated",
             "value" : "从边栏中选择"
+          }
+        }
+      }
+    },
+    "HTTPResponseError.InvalidHTTPResponse %lld" : {
+      "localizations" : {
+        "en" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "Invalid HTTP response code: %lld"
+          }
+        },
+        "zh-Hans" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "无效的HTTP回应状态码：%lld"
+          }
+        }
+      }
+    },
+    "HTTPResponseError.InvalidResponseType" : {
+      "localizations" : {
+        "en" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "Invalid response type"
+          }
+        },
+        "zh-Hans" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "无效的回应类型"
+          }
+        }
+      }
+    },
+    "HTTPResponseError.InvalidResponseType.Reason" : {
+      "localizations" : {
+        "en" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "The response is not a HTTPURLResponse object"
+          }
+        },
+        "zh-Hans" : {
+          "stringUnit" : {
+            "state" : "translated",
+            "value" : "回应不是一个HTTPURLResponse类型的对象"
           }
         }
       }
@@ -557,22 +589,6 @@
           "stringUnit" : {
             "state" : "translated",
             "value" : "（没有描述）"
-          }
-        }
-      }
-    },
-    "ScriptManager.TaskError.InvalidHTTPResponse %lld" : {
-      "localizations" : {
-        "en" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "Invalid HTTP response code: %lld"
-          }
-        },
-        "zh-Hans" : {
-          "stringUnit" : {
-            "state" : "translated",
-            "value" : "无效的HTTP回应状态码：%lld"
           }
         }
       }

App/Shared/Views/AddPluginView.swift
@@ -197,15 +197,12 @@ fileprivate enum Method : Int, CaseIterable {
 
 fileprivate enum TaskError: Error, LocalizedError {
     case externalLocationUnavailable
-    case invalidHTTPResponse(statusCode: Int)
     case invalidURL
     
     var errorDescription: String? {
         switch self {
         case .externalLocationUnavailable:
             return .init(localized: "AddPluginView.TaskError.ExternalLocationUnavailable")
-        case .invalidHTTPResponse(let statusCode):
-            return .init(localized: "AddPluginView.TaskError.InvalidHTTPResponse \(statusCode)")
         case .invalidURL:
             return .init(localized: "AddPluginView.TaskError.InvalidURL")
         }
@@ -215,8 +212,6 @@ fileprivate enum TaskError: Error, LocalizedError {
         switch self {
         case .externalLocationUnavailable:
             return .init(localized: "AddPluginView.TaskError.ExternalLocationUnavailable.Reason")
-        case .invalidHTTPResponse(let statusCode):
-            return HTTPURLResponse.localizedString(forStatusCode: statusCode)
         case .invalidURL:
             return .init(localized: "AddPluginView.TaskError.InvalidURL.Reason")
         }
@@ -296,18 +291,13 @@ fileprivate struct RemoteSection: View {
                 throw TaskError.invalidURL
             }
             
-            let (temporaryURL, response) = try await URLSession.shared.download(from: downloadURL)
+            let temporaryURL = try await URLSession.shared.download(from: downloadURL)
             
             let fileManager = FileManager.default
             defer {
                 if (self.pluginInformation == nil) {
                     try? fileManager.removeItem(at: temporaryURL)
                 }
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse else { return }
-            guard httpResponse.statusCode == 200 else {
-                throw TaskError.invalidHTTPResponse(statusCode: httpResponse.statusCode)
             }
             
             let content = try String(contentsOf: temporaryURL)

IntelStack.xcodeproj/project.pbxproj
@@ -26,6 +26,7 @@
 		84141CA62B638FE5005A07BC /* Plugin+Category.swift in Sources */ = {isa = PBXBuildFile; fileRef = 84141CA52B638FE5005A07BC /* Plugin+Category.swift */; };
 		84141CA72B638FE5005A07BC /* Plugin+Category.swift in Sources */ = {isa = PBXBuildFile; fileRef = 84141CA52B638FE5005A07BC /* Plugin+Category.swift */; };
 		84141CB12B63B205005A07BC /* AlertableModifier.swift in Sources */ = {isa = PBXBuildFile; fileRef = 84141CB02B63B205005A07BC /* AlertableModifier.swift */; };
+		84141CB52B63DDE5005A07BC /* URLSession+HTTP.swift in Sources */ = {isa = PBXBuildFile; fileRef = 84141CB42B63DDE5005A07BC /* URLSession+HTTP.swift */; };
 		842318862AB7053A000A0A09 /* IntelStackApp.swift in Sources */ = {isa = PBXBuildFile; fileRef = 842318852AB7053A000A0A09 /* IntelStackApp.swift */; };
 		842318882AB7053A000A0A09 /* ContentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 842318872AB7053A000A0A09 /* ContentView.swift */; };
 		8423188A2AB7053A000A0A09 /* Assets.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = 842318892AB7053A000A0A09 /* Assets.xcassets */; };
@@ -120,6 +121,7 @@
 		84141CA22B638874005A07BC /* MainScriptMetadata.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MainScriptMetadata.swift; sourceTree = "<group>"; };
 		84141CA52B638FE5005A07BC /* Plugin+Category.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Plugin+Category.swift"; sourceTree = "<group>"; };
 		84141CB02B63B205005A07BC /* AlertableModifier.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = AlertableModifier.swift; sourceTree = "<group>"; };
+		84141CB42B63DDE5005A07BC /* URLSession+HTTP.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "URLSession+HTTP.swift"; sourceTree = "<group>"; };
 		842318822AB7053A000A0A09 /* Intel Stack.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = "Intel Stack.app"; sourceTree = BUILT_PRODUCTS_DIR; };
 		842318852AB7053A000A0A09 /* IntelStackApp.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = IntelStackApp.swift; sourceTree = "<group>"; };
 		842318872AB7053A000A0A09 /* ContentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ContentView.swift; sourceTree = "<group>"; };
@@ -383,11 +385,11 @@
 				84D068DC2ABDB4D900EC6E7E /* FileConstants.swift */,
 				84141CA22B638874005A07BC /* MainScriptMetadata.swift */,
 				84D8E95C2AB731AE000234A9 /* Plugin.swift */,
+				84141CA52B638FE5005A07BC /* Plugin+Category.swift */,
 				84D8E9692AB87859000234A9 /* PluginMetadata.swift */,
 				84D8E95A2AB7312C000234A9 /* ScriptManager.swift */,
 				844AF6962ABC882500D90B0F /* ScriptManager+ExternalScripts.swift */,
 				842FFA4B2B57A1F4006DF29F /* UserScriptMetadataDecoder.swift */,
-				84141CA52B638FE5005A07BC /* Plugin+Category.swift */,
 			);
 			path = Data;
 			sourceTree = "<group>";
@@ -417,6 +419,7 @@
 		84D8E96C2AB87DE8000234A9 /* Extensions */ = {
 			isa = PBXGroup;
 			children = (
+				84141CB42B63DDE5005A07BC /* URLSession+HTTP.swift */,
 				8437CD152ABB3DEB008AF3A5 /* UserDefaults+Key.swift */,
 			);
 			path = Extensions;
@@ -617,6 +620,7 @@
 				84D8E96E2AB87DFB000234A9 /* Plugin+SwiftUI.swift in Sources */,
 				84D8E95D2AB731AE000234A9 /* Plugin.swift in Sources */,
 				84D068D92ABDB2EB00EC6E7E /* ModelContainer.swift in Sources */,
+				84141CB52B63DDE5005A07BC /* URLSession+HTTP.swift in Sources */,
 				842318882AB7053A000A0A09 /* ContentView.swift in Sources */,
 				84D8E96A2AB87859000234A9 /* PluginMetadata.swift in Sources */,
 				84141C9C2B6242D3005A07BC /* UpdateStatusEnvironmentKey.swift in Sources */,

commit cef35f288a013038e99ed382bae5fd7a8a8013ad

Fix url of downloading internal plugins - Bug introduced in 41498db

App/Shared/Data/ScriptManager+Task.swift
@@ -112,7 +112,8 @@ extension ScriptManager {
     func downloadInternalPlugin(_ filename: String, from channel: BuildChannel) async throws {
         let downloadURL = Self.websiteBuildURL
             .appending(path: channel.rawValue)
-            .appending(path: FileConstants.mainScriptFilename)
+            .appending(path: "plugins")
+            .appending(path: filename)
             .appendingPathExtension("user.js")
         let temporaryURL = try await URLSession.shared.download(from: downloadURL)
         