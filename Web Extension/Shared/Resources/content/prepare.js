79324df8b1d2b4c690609e887725f591c6ee5ef0
lucka-me <i@lucka.moe>
2024-05-26 19:53:41 +0800

    Replace the dismiss button with a close button on top of the screen

--- Web Extension/Shared/Resources/content/prepare.js
+++ Web Extension/Shared/Resources/content/prepare.js
@@ -14,12 +14,21 @@ async function execute() {
         return;
     }
 
-    // Create a dismiss button for details sheet
-    const toolboxNode = document.getElementById("toolbox");
-    const dismissButtonNode = document.createElement("a");
-    dismissButtonNode.textContent = "Dismiss"
-    dismissButtonNode.addEventListener("click", () => { show('map'); });
-    toolboxNode.appendChild(dismissButtonNode);
+    const playerStatNode = document.getElementById("playerstat");
+    playerStatNode.style.display = "flex";
+
+    // Create a dismiss button for info screen
+    const closeButtonNode = document.createElement("span");
+    closeButtonNode.textContent = "X";
+    closeButtonNode.title = "Close info screen";
+    closeButtonNode.style.border = "1px outset #20A8B1";
+    closeButtonNode.style.color = "#FFCE00";
+    closeButtonNode.style.fontSize = "16px";
+    closeButtonNode.style.padding = "4px";
+    closeButtonNode.style.margin = "3px 5px";
+    closeButtonNode.addEventListener("click", () => { show('map'); });
+
+    playerStatNode.prepend(closeButtonNode);
 }
 
 execute();
